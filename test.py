61c7709afc42f13671f36498de221f73c785a0ee
MJ <mjpin7@gmail.com>
2018-10-28 14:56:30 -0400

    Added shell context

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+# Creates a shell context for using the "flask shell" command. Mainly for testing in interpereter
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
