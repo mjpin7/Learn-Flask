899bc01ff4268fb7afb8f1de07647c54e9060e6b
MJ <mjpin7@gmail.com>
2018-10-28 09:59:20 -0400

    Added post functionality on login

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.forms import LoginForm
 
 # When on the paths '/' and '/index', run the index method
@@ -20,7 +20,12 @@ def index():
     return render_template('index.html', title="Home", user=user, posts=posts)
 
 # When on the '/login' path, run the login method to render the login page to the browser
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
+
     return render_template('login.html', title="Sign In", form=form)
