7d6963e63a29dee61e1434dd4d983c142e71330d
MJ <mjpin7@gmail.com>
2018-10-28 14:42:18 -0400

    Added db

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # Creates the flask app object as an instance of the clas Flask
 app = Flask(__name__)
@@ -7,4 +9,8 @@ app = Flask(__name__)
 # Tell flask to apply the configs from the config file
 app.config.from_object(Config)
 
-from app import routes
+# Database object and migration engine
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
