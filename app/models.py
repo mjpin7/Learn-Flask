300ca7a32a888576740457d7ec86ae3b0bc5c379
MJ <mjpin7@gmail.com>
2018-11-04 11:15:44 -0500

    Updated database

--- app/models.py
+++ app/models.py
@@ -13,6 +13,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    bio = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     # Method to tell how to print the objects of this class
     def __repr__(self):
