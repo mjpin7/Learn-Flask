c0b057f4e44ac6ffc19a6d45f781e88b38966f68
MJ <mjpin7@gmail.com>
2018-11-11 20:38:15 -0500

    Added error log handling

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,6 @@
 FLASK_APP=MyBlog.py
+MAIL_SERVER=smtp.googlemail.com
+MAIL_PORT=587
+MAIL_USE_TLS=1
+MAIL_USERNAME=<your-gmail-username>
+MAIL_PASSWORD=<your-gmail-password>
--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging 
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 # Creates the flask app object as an instance of the clas Flask
 app = Flask(__name__)
@@ -18,5 +21,37 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 # Sets the login view, this is used for forcing users to login to view certain pages
 login.login_view = 'login'
+
+# If the app is not in debug mode, then enable the email logger
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+
+        # Get the auth and security if set in env
+        auth = None
+        if app.config['MAIL_USERNAME' or app.config['MAIL_PASSWORD']]:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        
+        # Set the mail handler based on the env vars
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='MyBlog Error',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    # This creates an error log file in the logs directory
+    if not os.path.exists('log'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/myblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('MyBlog startup')
 
 from app import routes, models, errors
